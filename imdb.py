7cea56a2f9bfe9e0160bfeec061175e5ce30fcf6
Adam Dick <adam.c.dick@gmail.com>
2019-03-27 15:26:29 -0400

    added conditionals for parse movie

--- imdb.py
+++ imdb.py
@@ -2,20 +2,27 @@ def parse_movie(m_content):
     m = {}
 
     m_header = m_content.find('h3', class_ = 'lister-item-header')
-    m['title'] = m_header.find('a').get_text()
-    m['year'] = m_header.find('span', class_ = 'lister-item-year').get_text()
-    m['year'] = int(m['year'][1:5])
+    if m_header.find('a'):
+        m['title'] = m_header.find('a').get_text()
+    if m_header.find('span', class_ = 'lister-item-year'):
+        m['year'] = m_header.find('span', class_ = 'lister-item-year').get_text()
+        m['year'] = int(m['year'][1:5])
 
     m_muted = m_content.find('p', class_ = 'text-muted')
-    m['certificate'] = m_muted.find('span', class_ = 'certificate').get_text()
-    m['runtime'] = m_muted.find('span', class_ = 'runtime').get_text()
-    m['genre'] = m_muted.find('span', class_ = 'genre').get_text().strip()
-    m['genre'] = m['genre'].split(', ')
+    if m_muted.find('span', class_ = 'certificate'):
+        m['certificate'] = m_muted.find('span', class_ = 'certificate').get_text()
+    if m_muted.find('span', class_ = 'runtime'):
+        m['runtime'] = m_muted.find('span', class_ = 'runtime').get_text()
+    if m_muted.find('span', class_ = 'genre'):
+        m['genre'] = m_muted.find('span', class_ = 'genre').get_text().strip()
+        m['genre'] = m['genre'].split(', ')
 
     m_ratings = m_content.find('div', class_ = 'ratings-bar')
-    m['imdb_rating'] = float(m_ratings.find('div', class_ = 'ratings-imdb-rating').get_text().strip())
-    m['metascore'] = m_ratings.find('div', class_ = 'ratings-metascore').get_text().strip()
-    m['metascore'] = int(m['metascore'].split('\n')[0].strip())
+    if m_ratings.find('div', class_ = 'ratings-imdb-rating'):
+        m['imdb_rating'] = float(m_ratings.find('div', class_ = 'ratings-imdb-rating').get_text().strip())
+    if m_ratings.find('div', class_ = 'ratings-metascore'):
+        m['metascore'] = m_ratings.find('div', class_ = 'ratings-metascore').get_text().strip()
+        m['metascore'] = int(m['metascore'].split('\n')[0].strip())
 
     m_visible = m_content.find('p', class_ = 'sort-num_votes-visible')
     m_visible_children = m_visible.findChildren()
